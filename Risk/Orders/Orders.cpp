7982f7069bb570198bbfc7ce09caaa69b506632d
qandeelarshad <qandeelsphoto@gmail.com>
2020-09-30 14:34:00 -0400

    A1: Added Orders.h and Orders.cpp

---
+++ Risk/Orders/Orders.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "Orders.h"
+using namespace std;
+
+
+class Order { //parent class
+
+    //validate and execute methods to be inherited by the order subclasses
+    void validate(){
+
+    }
+    void execute(){
+    
+    }
+
+};
+
+
+class Deploy : Order {  //class Deploy inherits from Order
+
+ private:
+    int armies;  //number of armies to be deployed
+    string destination; //region where army is to be deployed
+
+    Deploy(int a, string o, string d){ //constructor for class Deploy
+        armies = a;
+        destination = d;
+    }
+
+    friend ostream& operator << (ostream &strm, const Deploy &d){
+
+        return strm << "Deploy " << d.armies << "armies to " << d.destination; 
+
+    }
+
+};
+
+//classes inheriting from order. yet to be implemented
+class Advance : Order {
+
+};
+
+class Bomb : Order {
+
+};
+
+class Blockade : Order {
+
+};
+
+class Airlift : Order {
+
+};
+
+class Negotiate : Order {
+
+};
---
+++ Risk/Orders/Orders.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#pragma once
+
+class Order{
+
+friend ostream& operator << (ostream &, const Order &); //stream insertion operator added to be inherited 
+
+//validate and execute methods to be inherited by the order subclasses
+void validate(); 
+void execute();
+
+};
+
+class Deploy : Order { //class Deploy inherits from Order
+
+    private:
+        int armies;  //number of armies to be deployed
+        string destination; //region where army is to be deployed
+
+    Deploy(int a, string o, string d); //constructor for class Deploy
+
+};
