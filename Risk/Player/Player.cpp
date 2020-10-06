11e154d7c8208067edd41cbde6d1757f119942f8
KevinHTrinh <kevin--trinh@outlook.com>
2020-10-05 22:18:52 -0400

    finished a prototype for Player class and driver, need Orders and Cards/Hands to be complete to finalize the player class.

---
+++ Risk/Player/Player.cpp
@@ -0,0 +1,30 @@
+#include "Player.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+Player::Player(list<Territory> territories, Hand hand, list<Orders> orders, int playerID) {
+       this.territories = new list<Territory>;
+	   this.hand = hand;
+	   this.orders = new list<Orders>;
+	   this.playerID = playerID;
+}
+
+list toDefend() {
+	list<Territory> toDefendTerritory;
+	// MAKE A LOOP WITH THE LOGIC WHEN territories is impletemented
+	// to check which have the flag to defend
+	return toDefendTerritory;
+}
+
+list toAttack() {
+	list<Territory> toAttackTerritory;
+	// MAKE A LOOP WITH THE LOGIC WHEN territories is impletemented
+	// to check which have the flag to attack
+	return toAttackTerritory;
+}
+
+void issueOrder(Orders order) {
+	this.orders.push_back(order);
+}
---
+++ Risk/Player/Player.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <string>
+
+#include "Orders/Orders.h"
+#include "Cards/Cards.h" // need part 3 to be done
+
+using namespace std;
+
+class Player {
+private:
+	list<Territory> _territories;
+	Hand _hand; // or to change to deck cards to validate when Part 5 is done
+	list<Orders> _orders;
+	int playerID;
+
+public:
+	list<Territory> toDefend();
+	list<Territory> toAttack();
+	void issueOrder(Orders order);
+};
