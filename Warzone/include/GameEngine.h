824696c6ffcb118cfb6cffc002d7c22b4b8f6417
KevinHTrinh <kevin--trinh@outlook.com>
2020-11-04 20:11:28 -0500

    initial commit for GameEngine files

--- Warzone/include/GameEngine.h
+++ Warzone/include/GameEngine.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#include "../include/Player.h"
+#include "../include/Map.h"
+#include <vector>
 #include <iostream>
 
+using namespace std;
 
+class GameEngine;
 
+class GameEngine {
+//private attributes for GameEngine class
+private:
+    Player player*;
+    Map map*;
+};
