caab493a54fd96faa19623464d699e9dcbc5a83e
tonylindorock <54872194+tonylindorock@users.noreply.github.com>
2020-11-15 20:54:15 -0500

    A2: Deleted GameEngine

--- Warzone/include/GameEngine.h
+++ Warzone/include/GameEngine.h
@@ -5,23 +5,6 @@
 #include "../include/Player.h"
 #include "../include/Orders.h"
 
-class GameEngine{
-    public:
-        const int MIN_PLAYER_NUM = 2;
-        const int MAX_PLAYER_NUM = 5;
-        GameEngine();
-        GameEngine(const GameEngine& gameEngine);
-        ~GameEngine();
-        void setPlayers(vector<Player*> *players);
-        void setMap(Map *map);
-    private:
-        int playerNum = MIN_PLAYER_NUM;
-        vector<Player*> *players;
-        Map *map;
-
-        friend class Startup;
-};
-
 class Startup{
     public:
         Startup(vector<Player*> *players, Map *map);
